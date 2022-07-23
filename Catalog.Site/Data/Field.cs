dde573860de9b6919dd1d5bdc67fc21196dc6b60
Tarxos <ftarxos@gmail.com>
2022-07-24 00:46:38 +0300

    add fields models

---
+++ Catalog.Site/Data/Field.cs
@@ -0,0 +1,21 @@
+﻿namespace Catalog.Site.Data;
+
+public abstract class Field
+{
+    public string? Name { get; set; }
+    public string? Value { get; set; }
+}
+
+public sealed class StringField : Field
+{
+}
+
+public sealed class NumberField : Field
+{
+    public int ExactValue => int.TryParse(Value, out var number) ? number : 0;
+}
+
+public sealed class FloatField : Field
+{
+    public double ExactValue => double.TryParse(Value, out var number) ? number : 0;
+}
