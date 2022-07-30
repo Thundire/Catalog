943233fb786cf3bfd20cab025c82016cd9dbacf3
Tarxos <ftarxos@gmail.com>
2022-07-30 16:32:28 +0300

    remove field types and its selection

--- Catalog.Site/Data/Field.cs
+++ Catalog.Site/Data/Field.cs
@@ -1,21 +1,7 @@
 ﻿namespace Catalog.Site.Data;
 
-public abstract class Field
+public class Field
 {
     public string? Name { get; set; }
     public string? Value { get; set; }
-}
-
-public sealed class StringField : Field
-{
-}
-
-public sealed class NumberField : Field
-{
-    public int ExactValue => int.TryParse(Value, out var number) ? number : 0;
-}
-
-public sealed class FloatField : Field
-{
-    public double ExactValue => double.TryParse(Value, out var number) ? number : 0;
 }
