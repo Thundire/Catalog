8b4742beadedd1d03f2395661ec66903fd372cf8
Tarxos <ftarxos@gmail.com>
2022-08-02 00:17:05 +0300

    add component for maps of data type

---
+++ Catalog.Site/Data/DataMap.cs
@@ -0,0 +1,14 @@
+﻿namespace Catalog.Site.Data;
+
+public class DataMap
+{
+    public static DataMap Empty { get; } = new (Array.Empty<string>());
+
+    public DataMap(string[] map)
+    {
+        Map = map;
+        ShortMap = map.Length > 0 ? string.Join(' ', map) : string.Empty;
+    }
+    public string ShortMap { get; set; }
+    public string[] Map { get; set; }
+}
